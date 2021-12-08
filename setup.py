9e78cc75dc1d3056a5218273956822263d8a70e9
Bren Moushall <bmoush@gmail.com>
2021-12-09 10:44:52 +1100

    Package not available?

--- setup.py
+++ setup.py
@@ -1,11 +1,11 @@
-from setuptools import setup, find_packages
+from setuptools import setup
 
 setup(
     name='flask-jwt-cognito',
     version='0.1',
     author='Brenainn Moushall',
     author_email='bmoush@gmail.com',
-    packages=find_packages(),
+    packages=["flask_jwt_cognito"],
     install_requires=[
         'flask',
         'flask-JWT-extended',
